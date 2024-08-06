15242355c5fc26209e555d975c4ce284a44d00d3
Charulata Sree Pokala <charulata014@gmail.com>
2024-08-06 19:55:14 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth/ble_controller.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("BLE SCANNER")),
+      body: GetBuilder<BleController>(
+        init: BleController(),
+        builder: (controller) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: StreamBuilder<List<ScanResult>>(
+                    stream: controller.scanResults,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            final data = snapshot.data![index];
+                            return Card(
+                              elevation: 2,
+                              child: ListTile(
+                                title: Text(data.device.name),
+                                subtitle: Text(data.device.id.id),
+                                trailing: Text(data.rssi.toString()),
+                                onTap: () => controller.connectToDevice(data.device),
+                              ),
+                            );
+                          },
+                        );
+                      } else {
+                        return const Center(child: Text("No Devices Found"));
+                      }
+                    },
+                  ),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    controller.scanDevices();
+                  },
+                  child: const Text("SCAN"),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
